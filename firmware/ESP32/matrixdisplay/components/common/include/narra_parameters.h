dcb138d0a6062aead1215830594abbbcf3054629
Kiogora <alois@bitsoko.io>
2017-08-17 10:46:26 +0300

    minor changes before sitching branch

--- firmware/ESP32/matrixdisplay/components/common/include/narra_parameters.h
+++ firmware/ESP32/matrixdisplay/components/common/include/narra_parameters.h
@@ -12,21 +12,23 @@ typedef struct
 {
     char* startup_msg;
 #ifdef ALLOW_STARTUP_UPDATE
+#pragma message("STARTUP UPDATE ALLOWED")
     char* startup_recovery;
     uint32_t* startup_crc_checksum;
 #endif
     char* shutdown_msg;
 #ifdef ALLOW_SHUTDOWN_UPDATE
+#pragma message("SHUTDOWN UPDATE ALLOWED")
     char* shutdown_recovery;
     uint32_t* shutdown_crc_checksum;
 #endif
     char* active_msg;
 #ifndef ALLOW_SIMPLE_ACTIVE_UPDATE
+#pragma message("SIMPLE ACTIVE MESSAGE UPDATE DISALLOWED")
     char* active_recovery;
     uint32_t* active_crc_checksum;
 #endif
 } System_variables;
-
 #ifdef __cplusplus
 }
 #endif
