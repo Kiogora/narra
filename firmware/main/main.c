9698b19d8c9ff89c2d4115e657ebc0d899e34df5
Kiogora <alois@bitsoko.io>
2017-09-15 02:34:20 +0300

    Move bleTask to main.c
    1. This will allow me to directly see how the tasks could interact.
    2. Implement the display task calling the ble task after succesful initialisation after reset state.
    3. Implement passing memory pointers to the initialisation structure variables. This should ideally be an array.
    4. This partly solves the TODO of the previous commit # 360c0e971072cfcf130eb8c8836a52d184987cfc
    5. TODO: Implement how the passed task variables will be visible to the BLE functions. Probably move attributes to the task code from global scope?

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -12,6 +12,14 @@
 #include "system_controller.h"
 #include "system_updater.h"
 
+typedef struct
+{
+
+    Matrix* matrixinstanceptr;
+    System_variables* system_variables;
+
+} Ble_task_parameters;
+
 void init_pin_interface(Matrix* matrixInstanceptr)
 {
 
@@ -36,6 +44,7 @@ void displayTask(void *pvParameters)
         .active_msg=NULL,
     };
 
+
     init_pin_interface(&matrix);
 
 /*    char* new_text="***WELCOME TO HEAVEN***";
@@ -44,25 +53,84 @@ presentation_t
     char* active_text="***VIVO-ENERGY©µ***";    
     system_update_active(active_text);
 */
-        if(matrix_init(&matrix, &system_variables, narra_speed_15) == ESP_OK)
+    if(matrix_init(&matrix, &system_variables, narra_speed_15) == ESP_OK)
+    {
+        Ble_task_parameters ble_task_parameters=
         {
-            for(;;)
-            {
+            .matrixinstanceptr=&matrix,
+            .system_variables=&system_variables;
+        };
+        /*Call xTaskCreate for the BLE task here*/
+        /*pass pointer to a structure of pointers to other structures-crazy*/
+        xTaskCreate(bleTask, "BleTask", 4096, (void*)&ble_task_parameters, 1, NULL);
+        for(;;)
+        {
 
-                matrix_display(&matrix, &system_variables, scroll);
-            }    
-        }
-        else
-        {
-            //implement pin and speed initialisation error exception handling here
-        }
+            matrix_display(&matrix, &system_variables, scroll);
+        }    
+    }
+    else
+    {
+        //implement pin and speed initialisation error exception handling here
+    }
+}
+
+void bleTask(void *pvParameters)
+{
+    esp_err_t ret;
+
+    // Initialize NVS.
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ret = esp_bt_controller_init(&bt_cfg);
+    if (ret)
+    {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s enable controller failed\n", __func__);
+        return;
+    }
+
+    ret = esp_bt_controller_enable(ESP_BT_MODE_BTDM);
+    if (ret)
+    {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s enable controller failed\n", __func__);
+        return;
+    }
+
+    ESP_LOGI(GATTS_TABLE_TAG, "%s init bluetooth\n", __func__);
+    ret = esp_bluedroid_init();
+    if (ret)
+    {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s init bluetooth failed\n", __func__);
+        return;
+    }
+    ret = esp_bluedroid_enable();
+    if (ret) 
+    {
+        ESP_LOGE(GATTS_TABLE_TAG, "%s enable bluetooth failed\n", __func__);
+        return;
+    }
+    string ="What the fuck are you saying bitch!!!!";
+    esp_ble_gatts_register_callback(gatts_event_handler);
+    esp_ble_gap_register_callback(gap_event_handler);
+    esp_ble_gatts_app_register(SYSTEM_APP_ID);
+    esp_ble_gatts_app_register(USAGE_APP_ID);
+
+    for(;;)
+    {
+        ;
+    }
 }
 
 void app_main(void)
 {
-    xTaskCreate(displayTask, "displayTask", 1024, NULL, 1, NULL );
-    xTaskCreate(bleTask, "bleTask", 1024, NULL, 1, NULL );
-    xTaskCreate(updaterTask, "updaterTask", 512, NULL, 2, NULL );
+    xTaskCreate(displayTask, "DisplayTask", 1024, NULL, 1, NULL );
 }
 //TODO: Move speed from system initialisation at start and implement in parameters in func write_speed/update_speed .
 
