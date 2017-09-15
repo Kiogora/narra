f2500ab679eedfcc71ceeeeb125d69a4a51d39d0
Kiogora <alois@bitsoko.io>
2017-09-16 02:35:48 +0300

    Remove the structure from within the display task for data integrity
    1. Allows the structure to be global thus is not stored on the stack frame.

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -14,15 +14,31 @@
 
 typedef struct
 {
-
     Matrix* matrixinstanceptr;
     System_variables* system_variables;
-
 } Ble_task_parameters;
+
+/*The below structures declared outside stack. Ensures validity between task frames and context switches*/
+Matrix matrix=
+{
+    .current_message=NULL,
+};
+
+System_variables system_variables =
+{
+    .startup_msg=NULL,
+    .shutdown_msg=NULL,
+    .active_msg=NULL,
+};
+/*Now define the structure*/
+Ble_task_parameters ble_task_parameters=
+{
+    .matrix_instance_ptr=&matrix,
+    .system_variables_ptr=&system_variables;
+};
 
 void init_pin_interface(Matrix* matrixInstanceptr)
 {
-
     matrixInstanceptr->serial_pin=GPIO_NUM_25;
     matrixInstanceptr->shift_pin=GPIO_NUM_26;
     matrixInstanceptr->latch_pin=GPIO_NUM_27;
@@ -32,19 +48,6 @@ void init_pin_interface(Matrix* matrixInstanceptr)
 
 void displayTask(void *pvParameters)
 {
-    Matrix matrix=
-    {
-        .current_message=NULL,
-    };
-
-    System_variables system_variables =
-    {
-        .startup_msg=NULL,
-        .shutdown_msg=NULL,
-        .active_msg=NULL,
-    };
-
-
     init_pin_interface(&matrix);
 
 /*    char* new_text="***WELCOME TO HEAVEN***";
@@ -55,17 +58,11 @@ presentation_t
 */
     if(matrix_init(&matrix, &system_variables, narra_speed_15) == ESP_OK)
     {
-        Ble_task_parameters ble_task_parameters=
-        {
-            .matrixinstanceptr=&matrix,
-            .system_variables=&system_variables;
-        };
         /*Call xTaskCreate for the BLE task here*/
         /*pass pointer to a structure of pointers to other structures-crazy*/
         xTaskCreate(bleTask, "BleTask", 4096, (void*)&ble_task_parameters, 1, NULL);
         for(;;)
         {
-
             matrix_display(&matrix, &system_variables, scroll);
         }    
     }
@@ -78,15 +75,17 @@ presentation_t
 void bleTask(void *pvParameters)
 {
     esp_err_t ret;
-
-    // Initialize NVS.
+    /* Initialise NVS*/
     ret = nvs_flash_init();
+
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES)
     {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK( ret );
+
+    /*TODO: cast the pvparameters, passed pointer to a valid type */
 
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     ret = esp_bt_controller_init(&bt_cfg);
