f1d57de77e384dc0016a7d7b91cdb22c7d4bd189
Kiogora <alois@bitsoko.io>
2017-09-19 00:39:26 +0300

    Add beacon_url.py
    1. Calls the mac functions made earlier and appends it to the bitsoko url.
    2. The response is JSON with a shortened url that can be used to build config_ble.h advertising data.

---
+++ utils/python/beacon_url.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import urllib
+import json
+
+#Contact bitsoko.co.ke with your device ID or list of.
+url = "https://bitsoko.co.ke/devices/?dID=240ac404a672"
+
+def get_short_url():
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    return data['shortUrl']
+
+if __name__=='__main__':
+    print get_short_url()
+
+
