afaaac7708bbea22d98b3ad9cba53bb17f1c9103
Kiogora <alois@bitsoko.io>
2017-10-07 18:50:20 +0300

    Update licence on source files

--- firmware/components/ble_api/include/config_ble.h
+++ firmware/components/ble_api/include/config_ble.h
@@ -1,3 +1,19 @@
+/*Author: alois mbutura<aloismbutura@gmail.com>
+
+ Copyright 2017 Bitsoko services LTD
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 #ifndef __CONFIG_BLE_H_
 #define __CONFIG_BLE_H_
 
@@ -47,7 +63,7 @@ uint8_t raw_scan_rsp_data[] =
 
 #define MANUFACTURER_NAME "BITSOKO_SERVICES"
 
-#define FW_VER_STRING "0.9.2"
+#define FW_VER_STRING "0.9.4"
 
 uint8_t  dev_id_val[8] = {0};
 
